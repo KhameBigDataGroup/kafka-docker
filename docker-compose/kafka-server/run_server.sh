cd4da16ef08b8264f4a75a952768e4995b439838
Mohammad Javad Shariati <mjshariati98@gmail.com>
2019-07-23 23:55:26 +0430

    Go to Docker-compose !
    try to run zookeeper and kafka-server together in docker-compose

---
+++ docker-compose/kafka-server/run_server.sh
@@ -0,0 +1,3 @@
+bin/kafka-server-start.sh config/server.properties >/root/kafka/server.log &
+
+watch uptime
---
+++ docker-compose/zookeeper/run_zookeeper.sh
@@ -0,0 +1,3 @@
+bin/zookeeper-server-start.sh config/zookeeper.properties >/root/kafka/zookeeper.log &
+
+watch uptime
